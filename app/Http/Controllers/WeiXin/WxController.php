a53a1966df79fca70164848c8bad2feef0edb651
ZMR <2417894482@qq.com>
2019-12-06 17:09:46 +0800

    验证微信接入

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -9,6 +9,21 @@ class WxController extends Controller
 {
     public function wechat()
     {
+        $token = '76b8fd82c97b80f0b4e23';    /开发提前设置好的 token
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        $echostr = $_GET["echostr"];
         
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+    
+        if( $tmpStr == $signature ){    //验证通过
+            echo $echostr;
+        }else{
+            die("not ok");
+        }
     }
 }
